805a19a7d676c448615bf4f83b8e02685bef1245
Ricardo Caballero <rcabamo@gmail.com>
2015-03-01 16:39:38 +0100

    Add view hierachy

--- Rubis/Modules/Add/User Interface/View/RCMTrainingAddViewController.m
+++ Rubis/Modules/Add/User Interface/View/RCMTrainingAddViewController.m
@@ -8,6 +8,26 @@
 
 #import "RCMTrainingAddViewController.h"
 
+#import "UIColor+Theme.h"
+
+@interface RCMTrainingAddViewController ()
+
+@property (nonatomic, weak) IBOutlet UIView *swimHeaderView;
+@property (nonatomic, weak) IBOutlet UIView *containerView;
+@property (nonatomic, weak) IBOutlet UIView *timeView;
+@property (nonatomic, weak) IBOutlet UIView *distanceView;
+
+@end
+
 @implementation RCMTrainingAddViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.containerView.backgroundColor = self.distanceView.backgroundColor = self.swimHeaderView.backgroundColor = [UIColor rb_redColor];
+    self.timeView.backgroundColor = [UIColor rb_redDisabledColor];
+    self.swimHeaderView.layer.cornerRadius = 5;
+}
 
 @end
